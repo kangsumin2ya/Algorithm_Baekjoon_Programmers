0d526412a253a205f3301853e1d53cb01992bff4
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-03-16 23:41:27 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+queue = deque()
+
+n = int(input())
+
+for i in range(n):
+    order = input().split()
+
+    if order[0] == 'push':
+        queue.append(order[1])
+
+    elif order[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+
+    elif order[0] == 'size':
+        print(len(queue))
+
+    elif order[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif order[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+
+    elif order[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
