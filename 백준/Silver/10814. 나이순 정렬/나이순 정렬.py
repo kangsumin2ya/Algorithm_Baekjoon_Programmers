392f79763af787b5bbb7a4f25e895e1a7951766f
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-06-25 20:52:04 +0900

    [Silver V] Title: 나이순 정렬, Time: 300 ms, Memory: 58428 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -5,15 +5,12 @@ input = sys.stdin.readline
 # 1. 정수 N 입력받기
 N = int(input())
 
-# 2. N만큼 반복하여 회원 이름, 나이를 인덱스와 함께 딕셔너리로 입력받기
-member_dict = {}
+# 2. member를 리스트로 입력받기
+members = [list(map(str, input().split())) for _ in range(N)]
 
-for idx in range(N):
-    member_dict[idx] = list(input().split())
+# 3. 정렬하기
+members.sort(key=lambda x: int(x[0]))
 
-# 3. 2가지 기준으로 sort()로 정렬
-sorted_member_dict = dict(sorted(member_dict.items(), key=lambda x: (int(x[1][0]), x[0])))
-
-# 4. 원하는 형식으로 출력
-for value in sorted_member_dict.values():
-    print(f'{value[0]} {value[1]}')
+# 4. 원하는 형식으로 출력하기
+for member in members:
+    print(*member)
