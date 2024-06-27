1f1f15f1c7b11cdf4840fee53166ed0ac0c2402f
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-06-27 20:36:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 368 ms, Memory: 153820 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -8,9 +8,5 @@ N = int(input())
 # 2. N개 정수 입력 받기
 nums = list(map(int, input().split()))
 
-# 3. 최솟값, 최댓값 구하기
-min_num = min(nums)
-max_num = max(nums)
-
-# 4. 공백 구분으로 출력
-print(f'{min_num} {max_num}')
+# 3. 공백 구분으로 출력
+print(min(nums), max(nums))
