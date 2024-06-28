86ac39ca2109e055434b03d17ed4a62747918c85
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-06-29 01:26:13 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -5,18 +5,24 @@ input = sys.stdin.readline
 # 1. a, b 입력받기
 a, b = map(int, input().split())
 
-# 2. 최대공약수 구하기
-def gcd(a, b):
-    for i in range(min(a, b), 0, -1):
-        if a % i == 0 and b % i == 0:
-            return i
+#### 직접 구현 시
+# # 2. 최대공약수 구하기
+# def gcd(a, b):
+#     for i in range(min(a, b), 0, -1):
+#         if a % i == 0 and b % i == 0:
+#             return i
+#
+# # 3. 최소공배수 구하기
+# def lcm(a, b):
+#     for i in range(max(a, b), (a*b)+1):
+#         if i % a == 0 and i % b == 0:
+#             return i
+#
+# print(gcd(a, b))
+# print(lcm(a, b))
 
-# 3. 최소공배수 구하기
-def lcm(a, b):
-    for i in range(max(a, b), (a*b)+1):
-        if i % a == 0 and i % b == 0:
-            return i
+#### math 이용
+import math
 
-print(gcd(a, b))
-
-print(lcm(a, b))
+print(math.gcd(a,b))
+print(math.lcm(a,b))
