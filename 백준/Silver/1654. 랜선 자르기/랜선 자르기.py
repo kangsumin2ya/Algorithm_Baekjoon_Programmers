9ef03564535803fc20c47c45ed347ae16a00bd7d
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-07-09 21:12:23 +0900

    [Silver II] Title: 랜선 자르기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -2,23 +2,34 @@ import sys
 
 input = sys.stdin.readline
 
+# 1. K, N 입력
 K, N = map(int, input().split())
 
+# 2. K번 반복해 길이 입력
 lengths = [int(input()) for _ in range(K)]
 
+# 3. 랜선 길이 오름차순 정렬
+lengths.sort()
+
+# 4. 이분 탐색 구현
+# 4-1. start, end 정의
 start = 1
-end = max(lengths)
+end = lengths[-1]
 
 while start <= end:
-    mid_value = (start + end) // 2
+    # 4-2. mid 정의
+    mid = (start + end) // 2
+
+    # 4-3. 각 길이에 나누어 얻은 몫의 합 계산
     count = 0
+    for length in lengths:
+        count += length // mid
 
-    for len in lengths:
-        count += len // mid_value
-
-    if count >= N:
-        start = mid_value + 1
+    # 4-4. 조건문에 따라 start, end 조절
+    if count >= N :
+        start = mid + 1
     else:
-        end = mid_value - 1
+        end = mid - 1
 
+# 5. 원하는 형식으로 출력
 print(end)
