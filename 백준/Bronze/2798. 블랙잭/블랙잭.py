52e0ba0b9239df81e6c224a355a7c013f4957537
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-07-11 23:30:30 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,23 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+# N, M 입력
+N, M = map(int, input().split())
+
+# N장의 카드 입력
+cards = list(map(int,input().split()))
+
+# 정답 저장 변수 선언
+ans = 0
+
+# N개 중 3개 고르는 조합 계산
+for com in combinations(cards, 3):
+    card_sum = sum(com)
+    # M을 넘지 않으면서 M에 가장 가까운 카드의 합 구하기
+    if card_sum <= M:
+        ans = max(card_sum, ans)
+        
+# 원하는 형태로 출력        
+print(ans)
