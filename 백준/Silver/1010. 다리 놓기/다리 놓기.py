8da102e326782152f95a74a3575e116d875ff0d5
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-07-12 20:13:16 +0900

    [Silver V] Title: 다리 놓기, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,20 +1,17 @@
 import sys
+import math
 
 input = sys.stdin.readline
 
+# 1. T 입력
 T = int(input())
 
+# 2. N, M 입력
 for _ in range(T):
     N, M = map(int, input().split())
 
-    D = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
+    # 3. 조합의 수 구하기
+    answer = math.comb(M, N)
 
-    for j in range(1, M+1):
-        D[1][j] = j
-        D[0][j] = 1
-
-    for i in range(2, N+1):
-        for j in range(i, M+1):
-            D[i][j] = D[i-1][j-1] + D[i][j-1]
-
-    print(D[N][M])
+    # 4. 원하는 형식으로 출력
+    print(answer)
