18fffa6f1c51734c00b49c09d582bf83c920da1f
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-07-14 21:31:12 +0900

    [Silver IV] Title: 바닥 장식, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -2,49 +2,37 @@ import sys
 
 input = sys.stdin.readline
 
-# 세로 크기 N, 가로 크기 M 입력
+# 1. N, M 입력
 N, M = map(int, input().split())
 
-# 바닥 장식 저장
-graph = []
+# 2. 바닥 장식 입력
+floor = [list(input().rstrip()) for _ in range(N)]
 
-# 세로 크기 N만큼 바닥 장식 입력
-for _ in range(N):
-    graph.append(list(input()))
+# 3. DFS 함수 구현
+def dfs(x, y):
+    # 3-1. '-'이면 방문처리 후 범위 내의 오른쪽, 왼쪽 확인
+    if floor[x][y] == '-':
+        floor[x][y] = 0
 
+        if y + 1 < M and floor[x][y + 1] == '-':
+            dfs(x, y + 1)
 
-# 바닥 장식 유형 조건을 가지고 재귀적으로 작동하는 DFS 구현
-def DFS(x, y):
-    if graph[x][y] == '|':
-        graph[x][y] = 0
+    # 3-3. '|'이면 방문처리 후 위, 아래 확인
+    elif floor[x][y] == '|':
+        floor[x][y] = 0
 
-        x_up = x + 1
-        x_down = x - 1
+        if x + 1 < N and floor[x + 1][y] == '|':
+            dfs(x + 1, y)
 
-        if 0 < x_up < N and graph[x_up][y] == '|':
-            DFS(x_up, y)
-
-        if 0 < x_down < N and graph[x_down][y] == '|':
-            DFS(x_down, y)
-
-    if graph[x][y] == '-':
-        graph[x][y] = 0
-
-        y_up = y + 1
-        y_down = y - 1
-
-        if 0 < y_up < M and graph[x][y_up] == '-':
-            DFS(x, y_up)
-
-        if 0 < y_down < M and graph[x][y_down] == '-':
-            DFS(x, y_down)
-
+# 4. 개수 저장 변수 정의
 count = 0
 
+# 5. 전체 바닥 장식 확인
 for i in range(N):
     for j in range(M):
-        if graph[i][j] != 0:
-            DFS(i, j)
+        if floor[i][j] != 0:
+            dfs(i, j)
             count += 1
 
+# 6. 원하는 형식으로 출력
 print(count)
