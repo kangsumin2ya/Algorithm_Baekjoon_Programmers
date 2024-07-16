3e9dbd2b4c038fa5c62c3ba4357f14fff5ee331b
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-07-16 17:08:06 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,53 +1,53 @@
-from collections import deque
-
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
+# 1. N, M, V 입력
 N, M, V = map(int, input().split())
 
-graph = []
-
-for _ in range(N+1):
-    graph.append([])
+# 2. 연결 정보 저장
+graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
     start, end = map(int, input().split())
-
     graph[start].append(end)
     graph[end].append(start)
 
+# 정점별 연결 정점 오름차순 정렬
 for i in range(N+1):
     graph[i].sort()
 
-def DFS(graph, start, visited):
-
-    visited[start] = True
-
+# 3. DFS/BFS 정의
+def dfs(graph, start, visited):
+    # 방문 처리
+    visited[start] = 1
+    # 결과 출력
     print(start, end=' ')
-
+    # 연결된 방문 안한 정점 방문
     for i in graph[start]:
         if not visited[i]:
-            DFS(graph, i, visited)
+            dfs(graph, i, visited)
 
-def BFS(graph, start, visited):
+def bfs(graph, start, visited):
     queue = deque([start])
-
-    visited[start] = True
-
+    # 방문 처리
+    visited[start] = 1
     while queue:
-        start = queue.popleft()
-        print(start, end=' ')
-
-        for i in graph[start]:
+        v = queue.popleft()
+        # 결과 출력
+        print(v, end=' ')
+        # 연결된 방문 안한 정점 방문
+        for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
-                visited[i] = True
+                visited[i] = 1
 
-visited = [False] * (N + 1)
-DFS(graph, V, visited)
+# 4. 방문 처리 리스트 정의 후, DFS/BFS 수행하며 원하는 결과 출력
+visited = [0] * (N+1)
+dfs(graph, V, visited)
 
 print()
 
-visited = [False] * (N + 1)
-BFS(graph, V, visited)
+visited = [0] * (N+1)
+bfs(graph, V, visited)
