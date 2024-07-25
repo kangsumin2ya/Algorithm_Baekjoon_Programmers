5f83fd1ca5571cb1bae01b95da465357d79d723c
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-07-25 23:19:33 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -6,18 +6,20 @@ input = sys.stdin.readline
 N = int(input())
 
 # 최소 봉지 수 초기화
-count = -1
+count = 0
 
-# 5kg 봉지 최대 개수부터 하나씩 줄여가며 검사
-for i in range(N // 5, -1, -1):
-    # 5kg 봉지 사용해 남은 무게 계산
-    remain = N - (i * 5)
-    # 남은 무게가 3으로 나누어 떨어지는지 확인
-    if remain % 3 == 0:
-        # 나누어 떨어지면 그 몫을 더해줌
-        count = i + (remain // 3)
-        # 원하는 값 찾았으니 종료
+# N을 줄여가며 반복
+while N >= 0:
+    # N이 5의 배수이면 몫 더해주기
+    if N % 5 == 0:
+        count += N // 5
+        # 정답 출력
+        print(count)
         break
+    # 나누어 떨어지지 않으므로 총 무게에서 3을 뺀 후 위의 조건문으로 돌아가기
+    N -= 3
+    count += 1
 
-# 정답 출력
-print(count)
+# N이 0으로 종료되지 않은 경우 실행
+else:
+    print(-1)
