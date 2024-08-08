3a8b195006afb6e896cce2ed0b0646a2390639ae
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-08-08 23:46:43 +0900

    [Silver I] Title: 부등호, Time: 132 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,45 +1,28 @@
-import sys
-from itertools import permutations
+def check(a, b, op):
+  if op == '<':
+    if a > b: return False
+  if op == '>':
+    if a < b: return False
+  return True
 
-input = sys.stdin.readline
+def dfs(cnt, num):
+  if cnt == k+1:
+    answer.append(num)
+    return
+  
+  for i in range(10):
+    if visited[i]: continue
 
-# 부등호 조건 만족하는지 확인하는 함수
-def check_sign(a, b, op):
-    if op == '<' and a < b:
-        return True
-    if op == '>' and a > b:
-        return True
-    return False
+    if cnt == 0 or check(num[cnt-1], str(i), signs[cnt-1]):
+      visited[i] = 1
+      dfs(cnt+1, num+str(i))
+      visited[i] = 0
 
-# k 입력
 k = int(input())
-
-# A 입력
-A = list(input().rstrip().split())
-
-# 0 ~ 9까지의 정수 리스트 만들기
-nums = [i for i in range(10)]
-
-# 새로 만든 정수 저장할 리스트 초기화
-new_nums = []
-
-# 0 ~ 9 중 k+1개 선택하는 순열 생성
-for per in permutations(nums, k+1):
-    check = True
-    for i in range(len(A)):
-        # 부등호 조건 불만족
-        if not check_sign(per[i], per[i + 1], A[i]):
-            check = False
-            break
-    # 부등호 조건 만족 시 최댓값, 최솟값 갱신
-    if check == True:
-        # 한 문자열로 만들기
-        n = ''.join(map(str, per))
-        new_nums.append(n)
-
-# 만들어진 정수 정렬
-new_nums.sort()
-
-# 결과 출력
-print(new_nums[-1])
-print(new_nums[0])
+signs = list(input().split())
+visited = [0]*10
+answer = []
+dfs(0, '')
+answer.sort()
+print(answer[-1])
+print(answer[0])
