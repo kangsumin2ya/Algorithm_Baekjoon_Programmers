5980178439ae8b332bfd23d076e4ee09f0f610bc
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-08-30 23:12:28 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1720 ms, Memory: 101160 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -10,7 +10,7 @@ def bfs(start):
     visited = [0] * (N + 1)
     # 방문 처리
     visited[start] = 1
-    # 거리 계산 리스트 정의
+    # 이동 거리를 저장할 리스트 정의
     distance = [-1] * (N + 1)
     # 시작점 초기화
     distance[start] = 0
@@ -34,7 +34,6 @@ N, M, K, X = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
 
 
-# M번 반복해 A, B 공백 구분 입력 (A -> B 단방향)
 for _ in range(M):
     A, B = map(int, input().split())
     # 연결 정보 입력
@@ -46,7 +45,7 @@ distances = bfs(X)
 # 최단 거리 도시 번호 저장 리스트 정의
 cities = [i for i in range(1, N+1) if distances[i] == K]
 
-# 정답 출력
+# 결과 출력
 if cities:
     for city in sorted(cities):
         print(city)
