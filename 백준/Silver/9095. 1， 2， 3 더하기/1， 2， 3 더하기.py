e49de993fa3d322b22976c87bd20c361392324a9
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-09-13 13:51:08 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -2,35 +2,24 @@ import sys
 
 input = sys.stdin.readline
 
-# 1. 테스트케이스 수 T를 입력받는다.
+# 테스트케이스 입력
 T = int(input())
 
-
 for _ in range(T):
-    # 2. T번 반복하여 n을 입력받는다.
+    # 정수 입력
     n = int(input())
 
-    # 3. dp테이블을 정의한다.
-    dp = [0] * (n+1)
+    # dp 테이블 정의
+    dp = [0] * 12
 
-    # 4. 점화식 계산이 필요 없는 경우를 고려하여 미리 계산해준다.
-    if n == 1:
-        print(1)
+    # 점화식 초기화
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
 
-    elif n == 2:
-        print(2)
+    # 그 이상은 점화식 통해 값 채우기
+    for i in range(4, 11):
+        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
 
-    elif n == 3:
-        print(4)
-
-    # 5. 점화식을 정의한다.
-    else:
-        dp[1] = 1   # 1
-        dp[2] = 2   # 1+1, 2
-        dp[3] = 4   # 1+1+1, 1+2, 2+1, 3
-
-        for i in range(4, n+1):
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-
-        # 6. d[n]을 출력한다.
-        print(dp[n])
+    # 결과 출력
+    print(dp[n])
