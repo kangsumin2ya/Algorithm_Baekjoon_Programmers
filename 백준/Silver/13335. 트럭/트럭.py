ea70b118db1fffbb83a9d2ff795eaf259fdcf3b2
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-09-20 23:10:20 +0900

    [Silver I] Title: 트럭, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -2,21 +2,33 @@ import sys
 
 input = sys.stdin.readline
 
+# n, w, L 입력
 n, w, L = map(int, input().split())
 
-truck_weights = list(map(int, input().split()))
+# 트럭 무게 입력
+trucks = list(map(int, input().split()))
 
-on_bridge = [0] * w
+# 다리 리스트 초기화
+bridge = [0] * w
+
+# 시간 계산 변수 정의
 time = 0
 
-while on_bridge:
+# 트럭 올리는 과정 진행
+while bridge:
+    # 시간 증가
     time += 1
-    on_bridge.pop(0)
+    # 빈자리 만들기
+    bridge.pop(0)
 
-    if truck_weights:
-        if sum(on_bridge) + truck_weights[0] <= L:
-            on_bridge.append(truck_weights.pop(0))
+    # 트럭 다 옮길 때까지 진행
+    if trucks:
+        if sum(bridge) + trucks[0] <= L:
+            # 더해서 최대하중 안넘으면 트럭 추가
+            bridge.append(trucks.pop(0))
         else:
-            on_bridge.append(0)
+            # 최대하중 넘으면 다리 위 제거
+            bridge.append(0)
 
+# 결과 출력
 print(time)
