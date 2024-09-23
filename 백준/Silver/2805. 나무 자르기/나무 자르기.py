d20ef47b7b933c1bc0d7c672fc42d1d11c0846ee
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2024-09-23 22:47:42 +0900

    [Silver II] Title: 나무 자르기, Time: 4188 ms, Memory: 150200 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,25 +2,36 @@ import sys
 
 input = sys.stdin.readline
 
+# N, M 입력
 N, M = map(int, input().split())
 
+# 나무 길이 입력
 heights = list(map(int, input().split()))
 
+# 이분탐색 범위 지정
 start = 0
-end = sum(heights)
+end = max(heights)
 
+# 이분탐색 수행
 while start <= end:
-    mid_value = (start + end) // 2
-    count = 0
+    # 중앙값 정의
+    mid = (start + end) // 2
+    # 잘린 나무 길이 합 저장 변수 정의
+    cut_trees = 0
 
-    for h in heights:
-        if h > mid_value:
-            count += h - mid_value
+    # 잘린 나무 길이 계산
+    for height in heights:
+        # 절단기 높이보다 높은 나무만 자름
+        if height > mid:
+            cut_trees += height - mid
 
-    if count >= M:
-        start = mid_value + 1
-
+    # 합이 적어도 M보다 큰지 확인
+    if cut_trees >= M:
+        # 크면 최댓값 구하기 위해 절단기 높이를 더 높여본다
+        start = mid + 1
+    # 합이 M보다 작으면 절단기 높이 더 줄인다
     else:
-        end = mid_value - 1
+        end = mid - 1
 
+# 결과 출력
 print(end)
