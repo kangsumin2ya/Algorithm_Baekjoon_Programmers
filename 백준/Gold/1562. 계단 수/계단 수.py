b076a9007ed367fecb4b3250e4de87f3554e0561
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2025-01-08 23:46:36 +0900

    [Gold I] Title: 계단 수, Time: 648 ms, Memory: 41060 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.py
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -13,13 +13,15 @@ for i in range(1, 10):
     dp[1][i][1 << i] = 1
 
 # dp 테이븛 채우기
-for i in range(2, N+1):
-    for j in range(10):
-        for k in range(1024):
-            if j > 0:
-                dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i-1][j-1][k]) % 1000000000
-            if j < 9:
-                dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i-1][j+1][k]) % 1000000000
+for n in range(2, N+1):
+    for i in range(10):
+        for bit in range(1024):
+            if i == 0:
+                dp[n][i][bit | (1 << i)] = (dp[n][i][bit | (1 << i)] + dp[n-1][i+1][bit]) % 1000000000
+            elif i == 9:
+                dp[n][i][bit | (1 << i)] = (dp[n][i][bit | (1 << i)] + dp[n-1][i-1][bit]) % 1000000000
+            else:
+                dp[n][i][bit | (1 << i)] = (dp[n][i][bit | (1 << i)] + dp[n-1][i+1][bit] + dp[n - 1][i - 1][bit]) % 1000000000
 
 # 결과 계산
 answer = sum(dp[N][j][1023] for j in range(10)) % 1000000000
