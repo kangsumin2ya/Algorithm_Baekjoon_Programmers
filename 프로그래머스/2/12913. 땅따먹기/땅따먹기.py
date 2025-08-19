9754d688e1cea4d3f5151ad849f2752fe9d50dd3
Sumin Kang <71876655+kangsumin2ya@users.noreply.github.com>
2025-08-19 23:21:29 +0900

    [level 2] Title: 땅따먹기, Time: 227.07 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -4,6 +4,6 @@ def solution(land):
     
     for i in range(1, N):
         for j in range(4):
-            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+            land[i][j] += max(land[i-1][k] for k in range(4) if k != j)
             
     return max(land[-1])
